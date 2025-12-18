e504be89dcd9922c6115aab7129dc877b884650d
qingmingzhang <qingming.zhang@seel.com>
2025-12-18 17:59:31 +0800

    feat(constants): add NetworkRequestStatueEnum for network request status management

---
+++ src/constants/network_request_statue_enum.ts
@@ -0,0 +1,13 @@
+export enum NetworkRequestStatueEnum {
+  /* eslint-disable no-bitwise */
+  Idle = 1 << 0,
+  Refreshing = 1 << 1,
+  LoadingMore = 1 << 2,
+  Loading = Refreshing | LoadingMore,
+  RefreshSuccess = 1 << 3,
+  LoadMoreSuccess = 1 << 4,
+  Success = RefreshSuccess | LoadMoreSuccess,
+  RefreshFailed = 1 << 5,
+  LoadMoreFailed = 1 << 6,
+  Failed = RefreshFailed | LoadMoreFailed,
+}
