7d90ec8464b2746035586a640e85636fb8e94b16
qingmingzhang <qingming.zhang@seel.com>
2025-12-22 11:37:27 +0800

    refactor(example): extract quote constants outside SettlementPage component

--- example/src/pages/settlement-page.tsx
+++ example/src/pages/settlement-page.tsx
@@ -13,185 +13,171 @@ import type { IQuotesRequest } from '../../../src/dto/IQuotesRequest';
 import type IQuotesResponse from '../../../src/dto/IQuotesResponse';
 import type { Domain } from '../../../src/ui/seel-wfp-info-view';
 
+const quoteEU: IQuotesRequest = {
+  session_id: '3b87ea2a6cecdb94bae186263feb9e7f',
+  type: 'debenhams-wfp',
+  cart_id: '3b87ea2a6cecdb94bae186263feb9e7f',
+  device_category: 'mobile',
+  device_id: '1737534673',
+  device_platform: 'iOS',
+  extra_info: {
+    shipping_fee: 10,
+  },
+  merchant_id: '20251217201204133696',
+  is_default_on: true,
+  line_items: [
+    {
+      quantity: 3,
+      variant_id: '10013-0000-319802',
+      is_final_sale: true,
+      product_id: '10013-0000-319802',
+      category_2: 'Decor',
+      requires_shipping: true,
+      product_title:
+        'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
+      category_1: 'Household Goods',
+      line_item_id: '11111',
+      price: 50,
+      sales_tax: 0,
+      image_urls: ['https:/example.com/image1', 'https:/example.com/image2'],
+      currency: 'USD',
+      variant_title:
+        'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
+      allocated_discounts: 0,
+      final_price: 50,
+      shipping_origin: {
+        country: 'US',
+        city: '',
+        state: '',
+        zipcode: '',
+      },
+    },
+    {
+      allocated_discounts: 1,
+      category_1: 'Household Goods',
+      category_2: 'Decor',
+      currency: 'USD',
+      final_price: '15.00',
+      image_urls: ['https:/example.com/image1', 'https:/example.com/image2'],
+      is_final_sale: true,
+      line_item_id: '22222',
+      price: 10,
+      product_id: '10013-0000-319803',
+      product_title: 'Williams Brand Allegro 2',
+      quantity: 3,
+      requires_shipping: true,
+      sales_tax: 6,
+      shipping_origin: {
+        country: 'US',
+        city: '',
+        state: '',
+        zipcode: '',
+      },
+      variant_id: '10013-0000-319803',
+      variant_title: 'Williams Brand Allegro 2',
+    },
+  ],
+  customer: {
+    customer_id: '1111',
+    email: 'xie@seel.com',
+  },
+  shipping_address: {
+    address_1: '7 Buswell Street',
+    city: 'Boston',
+    country: 'US',
+    state: 'MA',
+    zipcode: '02215',
+  },
+};
+const quoteUS: IQuotesRequest = {
+  session_id: '3b87ea2a6cecdb94bae186263feb9e7f',
+  type: 'debenhams-wfp',
+  cart_id: '3b87ea2a6cecdb94bae186263feb9e7f',
+  device_category: 'mobile',
+  device_id: '1737534673',
+  device_platform: 'iOS',
+  extra_info: {
+    shipping_fee: 10,
+  },
+  merchant_id: '20251211204195299886',
+  is_default_on: true,
+  line_items: [
+    {
+      quantity: 3,
+      variant_id: '10013-0000-319802',
+      is_final_sale: true,
+      product_id: '10013-0000-319802',
+      category_2: 'Decor',
+      requires_shipping: true,
+      product_title:
+        'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
+      category_1: 'Household Goods',
+      line_item_id: '11111',
+      price: 50,
+      sales_tax: 0,
+      image_urls: ['https:/example.com/image1', 'https:/example.com/image2'],
+      currency: 'USD',
+      variant_title:
+        'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
+      allocated_discounts: 0,
+      final_price: 50,
+      shipping_origin: {
+        country: 'US',
+        city: '',
+        state: '',
+        zipcode: '',
+      },
+    },
+    {
+      allocated_discounts: 1,
+      category_1: 'Household Goods',
+      category_2: 'Decor',
+      currency: 'USD',
+      final_price: '15.00',
+      image_urls: ['https:/example.com/image1', 'https:/example.com/image2'],
+      is_final_sale: true,
+      line_item_id: '22222',
+      price: 10,
+      product_id: '10013-0000-319803',
+      product_title: 'Williams Brand Allegro 2',
+      quantity: 3,
+      requires_shipping: true,
+      sales_tax: 6,
+      shipping_origin: {
+        country: 'US',
+        city: '',
+        state: '',
+        zipcode: '',
+      },
+      variant_id: '10013-0000-319803',
+      variant_title: 'Williams Brand Allegro 2',
+    },
+  ],
+  customer: {
+    customer_id: '1111',
+    email: 'xie@seel.com',
+  },
+  shipping_address: {
+    address_1: '7 Buswell Street',
+    city: 'Boston',
+    country: 'US',
+    state: 'MA',
+    zipcode: '02215',
+  },
+};
+
 export default function SettlementPage() {
   const [domain, setDomain] = useState<Domain>('');
   const initialRef: any = null;
   const seelWidgetRef = useRef<any>(initialRef);
-  // const onClickSetup = () => {
-  //   fetchNetworkData();
-  // };
   useEffect(() => {
-    const fetchNetworkData = () => {
-      const quoteEU: IQuotesRequest = {
-        session_id: '3b87ea2a6cecdb94bae186263feb9e7f',
-        type: 'debenhams-wfp',
-        cart_id: '3b87ea2a6cecdb94bae186263feb9e7f',
-        device_category: 'mobile',
-        device_id: '1737534673',
-        device_platform: 'iOS',
-        extra_info: {
-          shipping_fee: 10,
-        },
-        merchant_id: '20251217201204133696',
-        is_default_on: true,
-        line_items: [
-          {
-            quantity: 3,
-            variant_id: '10013-0000-319802',
-            is_final_sale: true,
-            product_id: '10013-0000-319802',
-            category_2: 'Decor',
-            requires_shipping: true,
-            product_title:
-              'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
-            category_1: 'Household Goods',
-            line_item_id: '11111',
-            price: 50,
-            sales_tax: 0,
-            image_urls: [
-              'https:/example.com/image1',
-              'https:/example.com/image2',
-            ],
-            currency: 'USD',
-            variant_title:
-              'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
-            allocated_discounts: 0,
-            final_price: 50,
-            shipping_origin: {
-              country: 'US',
-              city: '',
-              state: '',
-              zipcode: '',
-            },
-          },
-          {
-            allocated_discounts: 1,
-            category_1: 'Household Goods',
-            category_2: 'Decor',
-            currency: 'USD',
-            final_price: '15.00',
-            image_urls: [
-              'https:/example.com/image1',
-              'https:/example.com/image2',
-            ],
-            is_final_sale: true,
-            line_item_id: '22222',
-            price: 10,
-            product_id: '10013-0000-319803',
-            product_title: 'Williams Brand Allegro 2',
-            quantity: 3,
-            requires_shipping: true,
-            sales_tax: 6,
-            shipping_origin: {
-              country: 'US',
-              city: '',
-              state: '',
-              zipcode: '',
-            },
-            variant_id: '10013-0000-319803',
-            variant_title: 'Williams Brand Allegro 2',
-          },
-        ],
-        customer: {
-          customer_id: '1111',
-          email: 'xie@seel.com',
-        },
-        shipping_address: {
-          address_1: '7 Buswell Street',
-          city: 'Boston',
-          country: 'US',
-          state: 'MA',
-          zipcode: '02215',
-        },
-      };
-      const quoteUS: IQuotesRequest = {
-        session_id: '3b87ea2a6cecdb94bae186263feb9e7f',
-        type: 'debenhams-wfp',
-        cart_id: '3b87ea2a6cecdb94bae186263feb9e7f',
-        device_category: 'mobile',
-        device_id: '1737534673',
-        device_platform: 'iOS',
-        extra_info: {
-          shipping_fee: 10,
-        },
-        merchant_id: '20251211204195299886',
-        is_default_on: true,
-        line_items: [
-          {
-            quantity: 3,
-            variant_id: '10013-0000-319802',
-            is_final_sale: true,
-            product_id: '10013-0000-319802',
-            category_2: 'Decor',
-            requires_shipping: true,
-            product_title:
-              'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
-            category_1: 'Household Goods',
-            line_item_id: '11111',
-            price: 50,
-            sales_tax: 0,
-            image_urls: [
-              'https:/example.com/image1',
-              'https:/example.com/image2',
-            ],
-            currency: 'USD',
-            variant_title:
-              'Williams Brand Allegro 2 Model Digital Piano w\\/ Accessories',
-            allocated_discounts: 0,
-            final_price: 50,
-            shipping_origin: {
-              country: 'US',
-              city: '',
-              state: '',
-              zipcode: '',
-            },
-          },
-          {
-            allocated_discounts: 1,
-            category_1: 'Household Goods',
-            category_2: 'Decor',
-            currency: 'USD',
-            final_price: '15.00',
-            image_urls: [
-              'https:/example.com/image1',
-              'https:/example.com/image2',
-            ],
-            is_final_sale: true,
-            line_item_id: '22222',
-            price: 10,
-            product_id: '10013-0000-319803',
-            product_title: 'Williams Brand Allegro 2',
-            quantity: 3,
-            requires_shipping: true,
-            sales_tax: 6,
-            shipping_origin: {
-              country: 'US',
-              city: '',
-              state: '',
-              zipcode: '',
-            },
-            variant_id: '10013-0000-319803',
-            variant_title: 'Williams Brand Allegro 2',
-          },
-        ],
-        customer: {
-          customer_id: '1111',
-          email: 'xie@seel.com',
-        },
-        shipping_address: {
-          address_1: '7 Buswell Street',
-          city: 'Boston',
-          country: 'US',
-          state: 'MA',
-          zipcode: '02215',
-        },
-      };
+    const setup = () => {
       if (seelWidgetRef.current) {
         const quote = domain === 'EU' ? quoteEU : quoteUS;
         seelWidgetRef.current.setup(quote);
       }
     };
-    fetchNetworkData();
+    setup();
   }, [domain]);
 
   return (
