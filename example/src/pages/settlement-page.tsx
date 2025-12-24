4d28a431383fabc960216c687a6e32551d2747a4
qingmingzhang <qingming.zhang@seel.com>
2025-12-24 14:44:45 +0800

    feat(example): add opt-out expiration testing controls

--- example/src/pages/settlement-page.tsx
+++ example/src/pages/settlement-page.tsx
@@ -4,15 +4,18 @@ import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { SeelWFPWidget } from '../../../src';
+import { SeelWFPWidget, SeelWidgetSDK } from '../../../src';
 import type { Domain, IQuotesResponse } from '../../../src';
 
 import CartCell from '../components/cart-cell';
 import { mockQuoteEU as quoteEU, mockQuoteUS as quoteUS } from '../mocks';
+import { readOptOutExpiredTime } from '../../../src/utils/storage_util';
 
 export default function SettlementPage() {
   const [domain, setDomain] = useState<Domain>('EU');
   const [request, setRequest] = useState(domain === 'EU' ? quoteEU : quoteUS);
+  const [defaultOptedIn, setDefaultOptedIn] = useState(true);
+  const [optedValidTime, setOptedValidTime] = useState('');
   const initialRef: any = null;
   const seelWidgetRef = useRef<any>(initialRef);
   useEffect(() => {
@@ -24,7 +27,6 @@ export default function SettlementPage() {
     };
     setup();
   }, [request]);
-
   return (
     <SafeAreaView
       style={[defaultStyles.safeAreaContainer]}
@@ -52,38 +54,70 @@ export default function SettlementPage() {
         <SeelWFPWidget
           ref={seelWidgetRef}
           domain={domain}
-          defaultOptedIn={true}
-          onChangeValue={({
+          defaultOptedIn={defaultOptedIn}
+          onChangeValue={async ({
             optedIn,
             quotesResponse,
           }: {
             optedIn: boolean;
             quotesResponse?: IQuotesResponse;
-          }): void => {
-            console.log(optedIn, quotesResponse);
+          }) => {
+            console.debug(
+              'optedIn:\n\n',
+              optedIn,
+              'quotesResponse.is_default_on:',
+              quotesResponse?.is_default_on
+            );
+            if (optedIn === false) {
+              const _optedValidTime = await readOptOutExpiredTime();
+              setOptedValidTime(_optedValidTime.toString());
+              console.warn('optedValidTime:\n\n', _optedValidTime);
+            }
           }}
         />
       </View>
       <View style={[defaultStyles.container, defaultStyles.columnContainer]}>
-        <View style={[defaultStyles.rowContainer]} />
-        <TouchableOpacity
-          style={defaultStyles.button}
-          onPress={() => {
-            setDomain('EU');
-            setRequest(quoteEU);
-          }}
-        >
-          <Text>Setup EU</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={defaultStyles.button}
-          onPress={() => {
-            setDomain('US');
-            setRequest(quoteUS);
-          }}
-        >
-          <Text>Setup US</Text>
-        </TouchableOpacity>
+        <View style={[defaultStyles.rowContainer]}>
+          <TouchableOpacity
+            style={[defaultStyles.button, defaultStyles.centerContainer]}
+            onPress={() => {
+              setDomain('EU');
+              setRequest(quoteEU);
+            }}
+          >
+            <Text>Setup EU</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[defaultStyles.button, defaultStyles.centerContainer]}
+            onPress={() => {
+              setDomain('US');
+              setRequest(quoteUS);
+            }}
+          >
+            <Text>Setup US</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={[defaultStyles.rowContainer]}>
+          <TouchableOpacity
+            style={[defaultStyles.button, defaultStyles.centerContainer]}
+            onPress={async () => {
+              SeelWidgetSDK.shared.optOutExpiredTime = 10 * 1000;
+            }}
+          >
+            <Text>Setup Opt-out 10s</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[defaultStyles.button, defaultStyles.centerContainer]}
+            onPress={() => {
+              setDefaultOptedIn(!defaultOptedIn);
+            }}
+          >
+            <Text>Setup DefaultOptedIn to {!defaultOptedIn}</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text>optedValidTime: {optedValidTime}</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -103,14 +137,19 @@ const defaultStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  centerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   columnContainer: {
     flexDirection: 'column',
   },
   rowContainer: {
+    width: '100%',
     flexDirection: 'row',
   },
   button: {
-    width: 300,
-    height: 80,
+    flex: 1,
+    height: 48,
   },
 });
